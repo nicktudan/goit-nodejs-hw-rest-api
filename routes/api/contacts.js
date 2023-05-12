1bb81a572ed39763fd93fc603765a357a5b93597
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-05-12 17:17:12 +0200

    Сhanges file routes/api/contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,41 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const contactsService = require("../../models/contacts");
+
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  // console.log(req.params)
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      // return res.status(404).json({
+      //   message: `Contact with ${contactId} is not found`,
+      // });
+      const error = new Error(`Contact with ${contactId} is not found`);
+      error.status = 404;
+      throw error;
+    }
+    res.json(result);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({
+      message,
+    });
+      }
+  
 })
 
 router.post('/', async (req, res, next) => {
