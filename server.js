ce55ed7a0d0d0ba84d987bcb6d8f3e81f4981b6c
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-05-20 17:54:23 +0200

    add .env

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const DB_HOST =
-  "mongodb+srv://Olga:9DPLeBoUCBMitEQV@cluster0.xp7u4rd.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+const DB_HOST = process.env;
+
+mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000)
+    console.log("Database connection successful");
+    app.listen(3000);
   })
   .catch(error => {
     console.log(error.message);
