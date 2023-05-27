162d48ac053a61fd2c44fe20e0c24fd8c8ebe750
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-05-27 03:45:02 +0200

    change file controllers\auth.js
    add bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,20 @@
+const bcrypt = require('bcrypt');
+
 const { User } = require('../models');
 const { HttpErr } = require("../helpers");
 const { tryCatchWrapper } = require("../decorators");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpErr(409, "Email in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     email: newUser.email,
