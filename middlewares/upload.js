cbcbbd819cafe803dd6dcb5c684eda2dd42ca8df
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-06-02 12:11:27 +0200

    change file middlewares/upload.js
    add const fileFilter

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,17 +1,32 @@
 const multer = require("multer");
 const path = require("path");
 
+const { HttpErr } = require("../helpers");
+
 const tempDir = path.join(__dirname, "../", "temp");
 
 const multerConfig = multer.diskStorage({
-    destination: tempDir,
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: {
+    fileSize: 1024 * 1024
+  }
 });
 
+const fileFilter = (req, file, cb) => {
+  const { mimetype } = file;
+  console.log(mimetype);
+  if (mimetype !== "image/jpeg" || mimetype !== "image/png") {
+    cb(HttpErr(400, "File can have only .jpg or .png extension"), false);
+  }
+  cb(null, true);
+};
+
 const upload = multer({
-    storage: multerConfig
+  storage: multerConfig,
+  fileFilter,
 });
 
 module.exports = upload;
