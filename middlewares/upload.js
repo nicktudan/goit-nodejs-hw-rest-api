ee277fda49eeb52018ac9b858f61c9c810b86936
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-06-02 13:30:27 +0200

    fix

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -9,23 +9,26 @@ const multerConfig = multer.diskStorage({
   destination: tempDir,
   filename: (req, file, cb) => {
     cb(null, file.originalname);
-  },
-  limits: {
-    fileSize: 1024 * 1024
   }
 });
+
+const limits = {
+  fileSize: 1024 * 1024,
+};
 
 const fileFilter = (req, file, cb) => {
   const { mimetype } = file;
   console.log(mimetype);
-  if (mimetype !== "image/jpeg" || mimetype !== "image/png") {
+  if (mimetype === "image/jpeg" || mimetype === "image/png") {
+    cb(null, true);
+  } else {
     cb(HttpErr(400, "File can have only .jpg or .png extension"), false);
   }
-  cb(null, true);
 };
 
 const upload = multer({
   storage: multerConfig,
+  limits,
   fileFilter,
 });
 
