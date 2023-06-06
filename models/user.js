94f0ea84a29ef953c24f79561d2e05b8fc775e7a
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-06-06 21:48:12 +0200

    change models\user.js
    add verify and verificationToken

--- models/user.js
+++ models/user.js
@@ -3,28 +3,35 @@ const Joi = require("joi");
 
 const { handleMongooseError } = require('../helpers');
 
-const userSchema = new Schema(
-  {
-    password: {
-      type: String,
-      required: [true, "Set password for user"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-    },
-    subscription: {
-      type: String,
-      enum: ["starter", "pro", "business"],
-      default: "starter",
-    },
-    token: String,
-    avatarURL: {
-      type: String,
-      required: true,
-    },
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Set password for user"],
   },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: String,
+  avatarURL: {
+    type: String,
+    required: true,
+  },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, "Verify token is required"],
+  },
+},
   { versionKey: false, timestamps: true }
 );
 
