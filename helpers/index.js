887baea72a8c46bf06ac6f4185152f92b78ebaeb
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-06-09 17:41:35 +0200

    add helpers\sendEmail.js

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpErr = require("./HttpErr");
 const handleMongooseError = require('./handleMongooseError');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
   HttpErr,
   handleMongooseError,
+  sendEmail,
 };
